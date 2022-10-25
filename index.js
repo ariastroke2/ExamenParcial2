2253e2aae833fdc2706e4cd29f1e66e5754c32d5
ariastroke2 <0237568@up.edu.mx>
2022-10-24 19:31:28 -0500

    new file:   .gitignore 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   userData.js

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var userData = require("./userData");
+
+
+const app = express();
+const apiPort = 3003 ;
+
+//console.log(userData);
+
+const router = express.Router();
+
+userData = userData.userData;           // Se va directamente a la lista del archivo
+
+router.get("/get-user-data/:id", function(request, response){
+    try{
+        const {params: {id :input}} = request;
+        console.log(input);
+        user = userData.find(function(item){
+            console.log(item.id," =? ", input);
+            return input == item.id;
+        })
+        if(user==undefined){
+            response.send({
+                "ERROR":"404 - Usuario no existe"
+            });
+        }else{
+            response.send({
+                "status":200,
+                "user":{
+                    "fullName": user.firstName + " " + user.lastName + " " + user.maidenName,
+                    "email":user.email,
+                    "age":user.age,
+                    "address":user.address,
+                    "jobTitle":user.company.title,
+                }
+            });
+        }
+    }catch(error){
+        console.log(error);
+        response.send({
+            "ERROR":"500 - Algo ha salido mal"
+        });
+    }
+});
+
+router.put("/update-user-address/:id", function(request, response){
+    try{
+        const {params: {id :input}} = request;
+        console.log(input);
+        user = userData.find(function(item){
+            console.log(item.id," =? ", input);
+            return input == item.id;
+        })
+        if(user==undefined){
+            response.send({
+                "ERROR":"404 - Usuario no existe"
+            });
+        }else{
+            let idx = userData.indexOf(user);
+            user.address = request.body.address;
+            userData[idx] = user;
+            response.send({
+                "status":200,
+                "user":{
+                    "fullName": user.firstName + " " + user.lastName + " " + user.maidenName,
+                    "email":user.email,
+                    "age":user.age,
+                    "address":user.address,
+                    "jobTitle":user.company.title,
+                }
+            });
+        }
+    }catch(error){
+        console.log(error);
+        response.send({
+            "ERROR":"500 - Algo ha salido mal"
+        });
+    }
+});
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+app.use(bodyParser.json())
+app.use('/', router)
+
+app.listen(apiPort, () => console.log(`server running on port ${apiPort}`))
